5342004df9e765eb3a7a7e013119144616df7b04
Ely Shaffir <elyshaffir@gmail.com>
2022-05-26 21:29:33 +0300

    Added TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{any, borrow, collections::HashMap, fmt::Display, fs, io, io::Read, mem, path};
-
+// todo documentation for public interface.
 pub trait BufferData {
+    // todo add vertex to the name
     type DataType;
 
     fn buffer_attributes<'a>() -> &'a [wgpu::VertexAttribute];
@@ -15,20 +16,23 @@ pub trait BufferData {
 }
 
 fn load_shader_module(name: &str) -> String {
+    // todo configure from outside or remove completely and use full path (or relative) from shader.
     let base_path = path::PathBuf::from("src/shading");
     let module_path = base_path.join(name).with_extension("wgsl");
     if !module_path.is_file() {
+        // todo convert to Result<...> and don't panic.
         panic!("Shader not found: {:?}", module_path);
     }
 
     let mut module_source = String::new();
-    io::BufReader::new(fs::File::open(&module_path).unwrap())
+    io::BufReader::new(fs::File::open(&module_path).unwrap()) // todo error handling, never unwrap().
         .read_to_string(&mut module_source)
         .unwrap();
     let mut module_string = String::new();
 
-    let first_line = module_source.lines().next().unwrap();
+    let first_line = module_source.lines().next().unwrap(); // todo include should be possible everywhere and not just in the first line.
     if first_line.starts_with("//!include") {
+        // todo proper string constant for every macro ("include", "define") and extract the "//!" prefix.
         for include in first_line.split_whitespace().skip(1) {
             module_string.push_str(&*load_shader_module(include));
         }
@@ -39,12 +43,13 @@ fn load_shader_module(name: &str) -> String {
 }
 
 pub trait WGSLData {
+    // todo rename to WGSLType or something.
     fn string_definition(&self) -> String;
 }
 
 pub struct Shader {
-    name: String,
-    code: String,
+    name: String, // todo rename
+    code: String, // todo rename?
 }
 
 impl Shader {
@@ -54,12 +59,14 @@ impl Shader {
     }
 
     pub fn define_str(&mut self, name: &str, value: &str) -> &mut Self {
+        // todo change to generic define (not just strings). Also, see define_many to see if this method is deprecated.
         self.code = self.code.replace(name, value);
         self
     }
 
     pub fn define_struct_array<T: WGSLData>(&mut self, name: &str, structs: &Vec<&T>) -> &mut Self {
-        let type_name = any::type_name::<T>().split("::").last().unwrap();
+        // todo rename to define_constant_array or something
+        let type_name = any::type_name::<T>().split("::").last().unwrap(); // todo allow type name to be optionally specified in WGSLData.
         let array_length = structs.len();
         let mut string_definition = String::new();
 
@@ -78,6 +85,7 @@ impl Shader {
     }
 
     pub fn define_many<T: Display>(&mut self, defines: &HashMap<&String, T>) -> &mut Self {
+        // todo rename to define and have as only way to define constants (ie. deprecate define_str)
         let type_name = any::type_name::<T>();
         for (name, value) in defines.iter() {
             self.define_str(name, &format!("{type_name}({value})"));
@@ -86,6 +94,7 @@ impl Shader {
     }
 
     pub fn load(&self) -> wgpu::ShaderModuleDescriptor {
+        // todo rename to build() or some other builder pattern name
         wgpu::ShaderModuleDescriptor {
             label: Some(&self.name),
             source: wgpu::ShaderSource::Wgsl(borrow::Cow::Borrowed(&self.code)),
@@ -93,6 +102,6 @@ impl Shader {
     }
 
     fn define_once(&mut self, name: &str, value: &str) -> &mut Self {
-        self.define_str(&format!("//!define {name}"), value)
+        self.define_str(&format!("//!define {name}"), value) // todo see //!include and follow suite.
     }
 }
