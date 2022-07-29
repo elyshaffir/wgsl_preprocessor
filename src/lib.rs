d71a9dc3fffa1bc159473e51e8c388993717e4e9
Ely Shaffir <elyshaffir@gmail.com>
2022-07-29 17:02:13 +0300

    Crate documentation and package metadata added

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,111 @@
+/*!
+This crate provides a library for performing similar actions to what is expected from a preprocessor in WGSL.
+Since WGSL [will not have a preprocessor](https://github.com/gpuweb/gpuweb/issues/568) at least for version 1.0,
+this crate provides solutions to some common problems like including shader files and defining constants from
+Rust code.
+
+# Example: Include Multiple Shader Files
+
+Here are the contents of the three shader files in this example (there are blank lines at the end of each included file):
+`test_shaders/main.wgsl`:
+```wgsl
+//!include test_shaders/included.wgsl test_shaders/included2.wgsl
+```
+`test_shaders/included.wgsl`:
+```wgsl
+struct TestStruct {
+	test_data: vec4<f32>;
+};
+
+```
+`test_shaders/included2.wgsl`:
+```wgsl
+struct AnotherTestStruct {
+	another_test_data: vec3<u32>;
+};
+
+```
+After building `main.wgsl`, it's compiled contents would be identical to:
+```wgsl
+struct TestStruct {
+	test_data: vec4<f32>;
+};
+struct AnotherTestStruct {
+	another_test_data: vec3<u32>;
+};
+
+```
+It is important to note that `test_shaders/main.wgsl` could also contain:
+```wgsl
+//!include test_shaders/included.wgsl
+//!include test_shaders/included2.wgsl
+```
+The result would be the same.
+
+# Example: Defining a Constant Struct Array
+
+Let's say some color constants are calculated before shader compile time and should be injected into the
+code for performance reasons.
+`main.wgsl` would contain:
+```wgsl
+//!define STRUCT_ARRAY
+```
+In the Rust code, `Struct` is defined, and given an implementation of [`WGSLType`] it can be translated to
+a WGSL struct with a single `vec4<f32>` member named `data`.
+The Rust code building and compiling the shaders will contain:
+```ignore
+struct Struct {
+	pub data: [f32; 4],
+}
+impl WGSLType for Struct {
+	fn type_name() -> String {
+		"Struct".to_string()
+	}
+
+	fn string_definition(&self) -> String {
+		format!("{}(vec4<f32>({:?}))", Self::type_name(), self.data)
+			.replace(&['[', ']'], "")
+	}
+}
+```
+After building and compiling `main.wgsl` with the following array definition:
+```ignore
+ShaderBuilder::new("main.wgsl")
+	.unwrap()
+	.put_array_definition(
+		"STRUCT_ARRAY",
+		&vec![
+			&Struct {
+				data: [1.0, 2.0, 3.0, 4.0]
+			},
+			&Struct {
+				data: [1.5, 2.1, 3.7, 4.9]
+			}
+		]
+	)
+```
+The compiled contents would be identical to:
+```wgsl
+var<private> STRUCT_ARRAY: array<Struct, 2> = array<Struct, 2>(Struct(vec4<f32>(1.0, 2.0, 3.0, 4.0)),Struct(vec4<f32>(1.5, 2.1, 3.7, 4.9)),);
+```
+
+# Crate features
+
+### Inserting Arrays of Suitable Lengths as Vectors
+
+By default, none of the following features are enabled.
+* **array_vectors** -
+  When enabled, implementations of [`WGSLType`] are compiled for all array types of suitable lengths and scalar types.
+  This feature forces the translation of (for example) `[f32; 4]` to the WGSL type `vec4<f32>` in methods like [`ShaderBuilder::put_array_definition`].
+* **cgmath_vectors** -
+  This feature is similar to **array_vectors** but with [`cgmath`] vector objects like [`cgmath::Vector3<u32>`]
+  which would be translated to `vec3<u32>`.
+*/
 use std::{any, borrow, collections::HashMap, mem, path};
 
 const INSTRUCTION_PREFIX: &str = "//!";
 const INCLUDE_INSTRUCTION: &str = const_format::concatcp!(INSTRUCTION_PREFIX, "include");
 const DEFINE_INSTRUCTION: &str = const_format::concatcp!(INSTRUCTION_PREFIX, "define");
-
-// todo use crate type_info to implement WGSLType for structs automatically (attribute). fields need to implement WGSLType recursively.
 
 /// Type for data types that would be later put in a vertex buffer.
 pub trait VertexBufferData
@@ -71,10 +172,10 @@ impl WGSLType for wgsl_type {
 	}
 }
 
-#[cfg(all(feature = "cgmath_vectors", feature = "array_vectors"))]
-compile_error!(
-	"Feature \"cgmath_vectors\" and feature \"array_vectors\" cannot be enabled at the same time."
-);
+// #[cfg(all(feature = "cgmath_vectors", feature = "array_vectors"))]
+// compile_error!(
+// 	"Feature \"cgmath_vectors\" and feature \"array_vectors\" cannot be enabled at the same time."
+// );
 
 #[cfg(feature = "array_vectors")]
 #[duplicate::duplicate_item(wgsl_type; [[u32; 2]]; [[i32; 2]]; [[f32; 2]]; [[u32; 3]]; [[i32; 3]]; [[f32; 3]]; [[u32; 4]]; [[i32; 4]]; [[f32; 4]])]
@@ -136,6 +237,7 @@ impl WGSLType for bool {
 /// Wraps shader code, changes it and builds it into a [`wgpu::ShaderModuleDescriptor`].
 pub struct ShaderBuilder {
 	/// String with the current WGSL source.
+	/// It is marked public for debugging purposes.
 	pub source_string: String,
 	source_path: String,
 }
@@ -451,10 +553,10 @@ mod tests {
 					"VECTOR_ARRAY",
 					&vec![&[1.0, 2.0, 3.0, 4.0], &[1.5, 2.1, 3.7, 4.9]]
 				)
-				.code,
+				.source_string,
 			ShaderBuilder::new("test_shaders/put_array_definition_vectors_processed.wgsl")
 				.unwrap()
-				.code
+				.source_string
 		)
 	}
 
